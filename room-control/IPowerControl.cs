afed7738dcfac4252e362c2b57bea39752659417
phonegi <phoneguy@buler.net>
2015-10-19 09:24:35 -0400

    working

---
+++ room-control/IPowerControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomControl {
+    interface IPowerControl {
+        public void PowerOn();
+        public void PowerOff();
+        
+    }
+}
